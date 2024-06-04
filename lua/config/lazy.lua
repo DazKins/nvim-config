935b15c855d8aff8f47c7cfda608824d344d3b0d
DazKins <david.atkins1998@gmail.com>
2024-06-05 01:01:10 +0800

    Add colorschemes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,6 @@ require("lazy").setup({
     lazy = false,
     version = false,
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true },
   performance = {
     rtp = {
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neanias/everforest-nvim",
+  },
+  {
+    "ellisonleao/gruvbox.nvim"
+  },
+  {
+    "folke/tokyonight.nvim"
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "tokyonight",
+    },
+  }
+}
