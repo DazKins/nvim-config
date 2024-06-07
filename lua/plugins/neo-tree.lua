ab79dd9723245d4733c356861cb5298ea2012e77
DazKins <david.atkins1998@gmail.com>
2024-06-07 16:30:10 +0800

    Collapse empty dirs

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,11 +7,13 @@ return {
         hide_dotfiles = false,
         hide_gitignored = false,
         hide_by_name = {
-          '.git',
-          '.DS_Store',
+          ".git",
+          ".DS_Store",
         },
         never_show = {},
-      }
-    }
-  }
+      },
+      group_empty_dirs = true,
+      scan_mode = "deep",
+    },
+  },
 }
